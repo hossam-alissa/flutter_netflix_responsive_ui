d34af7b5f4d3aa5d7453e1afa91d9e228ebaf605
hossam.alissa <hossam.alissa@yahoo.com>
2021-02-21 15:30:13 +0200

    ADD color in AppBar HomeScr

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ScrollController _scrollController;
   double _scrollOffset = 0.0;
 
+  @override
+  void initState() {
+    _scrollController = ScrollController()..addListener(() {
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
@@ -21,6 +37,12 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 50.0),
         child: CustomAppBar(scrollOffset: _scrollOffset,),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(child: Container(height: 1000.0,color: Colors.blue,),)
+        ],
       ),
     );
   }
